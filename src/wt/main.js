8f8918ae1bfdebf553a5fd63c6eb5f8049e3ecc4
Egor Semenov <egor_semenov@epam.com>
2025-04-27 23:40:23 +0400

    feat: implement calculating fib number with workers

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,4 +1,39 @@
+import { cpus } from 'os'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+import { Worker } from 'worker_threads'
+
+const currentDir = dirname(fileURLToPath(import.meta.url))
+
+const workerFileName = 'worker.js'
+const workerPath = join(currentDir, workerFileName)
+
+const cpuCores = cpus().length
+
+const createWorker = (dataToProcess) => (
+    new Promise((resolve) => {
+        const worker = new Worker(workerPath, { workerData: dataToProcess})
+    
+        worker.on('message', (data) => {
+            resolve(data)
+        })
+    })
+)
+
+const fibNToStartFrom = 10
+
 const performCalculations = async () => {
+    const promises = []
+    for (let i = 0; i < cpuCores; i++) {
+        const dataToProcess = fibNToStartFrom + i
+
+        const fibN = createWorker(dataToProcess)
+        promises.push(fibN)
+    } 
+
+
+    const res = await Promise.all(promises)
+    console.log('res', res)
     // Write your code here
 };
 
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,26 @@
-// n should be received from main thread
+import { parentPort, workerData } from 'worker_threads'
+
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
+const KNOWN_STATUS = {
+    RESOLVED: 'resolved',
+    ERROR: 'error'
+}
+
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+    try {
+        const result = nthFibonacci(workerData)
+    
+        parentPort.postMessage({
+            status: KNOWN_STATUS.RESOLVED,
+            value: result,
+        })
+    } catch {
+        parentPort.postMessage({
+            status: KNOWN_STATUS.ERROR,
+            value: null,
+        })
+    }
 };
 
 sendResult();
