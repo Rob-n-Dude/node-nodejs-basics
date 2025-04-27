84590787c2090bfc437e294355c16e1276835ef9
Egor Semenov <egor_semenov@epam.com>
2025-04-27 15:20:53 +0400

    feat: implement create file feature

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,11 @@
+import { fileURLToPath} from 'url'
+import { dirname } from 'path';
+
+
+const currentFilePath = fileURLToPath(import.meta.url)
+const __dirname = dirname(fileURLToPath)
+
+
 const copy = async () => {
     // Write your code here 
 };
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,28 @@
+import { open } from 'fs/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const currentFilePath = fileURLToPath(import.meta.url)
+const __dirname = dirname(currentFilePath)
+
+const FILE_NAME = join(__dirname, './files/fresh.txt')
+const CONTENT = 'I am fresh and young'
+
+const ALREADY_EXISTED_FILE_ERROR_CODE = 'EEXIST'
+const EXISTED_FILE_ERROR_MESSAGE = 'FS operation failed'
+
 const create = async () => {
-    // Write your code here 
+    try {
+        const file = await open(FILE_NAME, 'wx')
+
+        await file.write(CONTENT)
+
+        await file.close()
+    } catch (e) {
+        if (e.code === ALREADY_EXISTED_FILE_ERROR_CODE) {
+            throw new Error(EXISTED_FILE_ERROR_MESSAGE)
+        }
+    }
 };
 
 await create();
