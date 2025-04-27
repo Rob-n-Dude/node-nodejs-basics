5c52cb286f5b7f958752030cc95a859f14b77052
Egor Semenov <egor_semenov@epam.com>
2025-04-27 17:38:06 +0400

    feat: implement cli read env feature

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,16 @@
 const parseEnv = () => {
-    // Write your code here 
+    const targetPrefix = 'RSS_'
+
+    const envs = Object.entries(process.env).reduce((acc, [ key, value ]) => {
+        if (!key.startsWith(targetPrefix)) {
+            return acc
+        }
+
+        acc.push(`${key}=${value}`)
+        return acc
+    }, []).join('; ')
+
+    console.log(envs)
 };
 
 parseEnv();
