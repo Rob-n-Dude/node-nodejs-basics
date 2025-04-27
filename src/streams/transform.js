e945845d18d10bc3df3a866b51025dc56f2e8de5
Egor Semenov <egor_semenov@epam.com>
2025-04-27 20:53:12 +0400

    feat: implement transform to stdout feature

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
+import { Transform } from 'stream'
+
+const reverseTransform = new Transform({
+    transform: (data, encoding, callback) => {
+        const chunk = data.toString()
+
+        const withoutEndings = chunk.replace(/[\r\n]+/g, '');
+
+        const reversed = withoutEndings.split('').reverse().join('') + '\n'
+        callback(null, reversed)
+    }
+})
+
 const transform = async () => {
-    // Write your code here 
+    process.stdin.pipe(reverseTransform).pipe(process.stdout)
 };
 
 await transform();
