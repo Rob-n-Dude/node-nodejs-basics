2a4153325967624ce62432d55a6da883669db0cb
Egor Semenov <egor_semenov@epam.com>
2025-04-27 18:26:51 +0400

    feat: implement parseArgs feature

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,29 @@
+const isArg = (string) => {
+    return string.startsWith('--')
+}
+
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2)
+
+    const argIndexes = args.reduce((a, c, i) => {
+        if (!isArg(c)) {
+            return a
+        }
+
+        a.push(i)
+        return a
+    }, [])
+
+    return argIndexes.map((index) => {
+        const argName = args[index]
+        const argValue = args[index + 1]
+
+        if (!argValue) {
+            return
+        }
+
+        return `${argName} is ${argValue}`
+    }).filter(Boolean).join(', ')
 };
 
 parseArgs();
