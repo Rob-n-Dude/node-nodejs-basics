54f0153df044804c057e66bf599264c5be0e74c0
Egor Semenov <egor_semenov@epam.com>
2025-04-27 16:04:31 +0400

    feat: implement copy feature

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,13 +1,42 @@
 import { fileURLToPath} from 'url'
-import { dirname } from 'path';
-
+import { dirname, join } from 'path';
+import { copyFile, mkdir, readdir } from 'fs/promises';
 
 const currentFilePath = fileURLToPath(import.meta.url)
-const __dirname = dirname(fileURLToPath)
+const __dirname = dirname(currentFilePath)
 
+const targetDirName = 'files_copy'
+const sourceDirName = 'files'
+
+const ALREADY_EXISTED_FILE_ERROR_CODE = 'EEXIST'
+const EXISTED_FILE_ERROR_MESSAGE = 'FS operation failed'
+
+const NO_SUCH_FILE_ERROR = 'ENOENT'
 
 const copy = async () => {
-    // Write your code here 
+    try {
+        await mkdir(join(__dirname, targetDirName))
+
+    } catch (e) {
+        if (e.code === ALREADY_EXISTED_FILE_ERROR_CODE) {
+            throw new Error(EXISTED_FILE_ERROR_MESSAGE)
+        }
+    }
+
+    try {
+        const sourceFiles = await readdir(join(__dirname, sourceDirName))
+
+        for await(const fileName of sourceFiles) {
+            const sourcePath = join(__dirname, sourceDirName, fileName)
+            const targetPath = join(__dirname, targetDirName, fileName)
+
+            await copyFile(sourcePath, targetPath)
+        }
+    } catch (e) {
+        if (e.code === NO_SUCH_FILE_ERROR) {
+            throw new Error(EXISTED_FILE_ERROR_MESSAGE)
+        }
+    }
 };
 
 await copy();
