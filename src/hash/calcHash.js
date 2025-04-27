740fcddd77c9f19bac3f91ab77c702c1692f623e
Egor Semenov <egor_semenov@epam.com>
2025-04-27 19:10:27 +0400

    feat: implement calcHash feature

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,36 @@
+import { fileURLToPath  } from 'url'
+import { dirname, join } from 'path';
+import { createHash } from 'crypto'
+import { createReadStream } from 'fs';
+
+const currentDir = dirname(fileURLToPath(import.meta.url))
+
+const targetFile = './files/fileToCalculateHashFor.txt'
+const targetFilePath = join(currentDir, targetFile)
+
+const ALG = 'SHA256'
+
 const calculateHash = async () => {
-    // Write your code here 
+    return new Promise((resolve, reject) => {
+        const fileStream = createReadStream(targetFilePath)
+        const hash = createHash(ALG)
+    
+        fileStream.on('data', (data) => {
+            hash.update(data)
+        })
+    
+        fileStream.on('end', () => {
+            const hashResult = hash.digest('hex')
+            console.log('hash:', hashResult)
+
+            resolve(hashResult)
+        })
+    
+        fileStream.on('error', (error) => {
+            reject(error)
+        })
+    })
 };
+
 
 await calculateHash();
