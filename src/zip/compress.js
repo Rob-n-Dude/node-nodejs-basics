55c79d9ac2c5c1a1e95aca76b173c041bc6ecc1f
Egor Semenov <egor_semenov@epam.com>
2025-04-27 21:15:09 +0400

    feat: implement gzip compression and decompression feature

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,23 @@
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path';
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+
+const currentDir = dirname(fileURLToPath(import.meta.url))
+
+const sourceFile = './files/fileToCompress.txt'
+const targetFile = './files/archive.gz'
+
+const sourceFilePath = join(currentDir, sourceFile)
+const targetFilePath=  join(currentDir, targetFile)
+
 const compress = async () => {
-    // Write your code here 
+    const readFileStream = createReadStream(sourceFilePath)
+    const writeFileStream = createWriteStream(targetFilePath)
+
+    const zipper = createGzip()
+
+    readFileStream.pipe(zipper).pipe(writeFileStream)
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,23 @@
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path';
+import { createReadStream, createWriteStream } from 'fs';
+import { createGunzip } from 'zlib';
+
+const currentDir = dirname(fileURLToPath(import.meta.url))
+
+const targetFile = './files/fileToCompress.txt'
+const sourceFile = './files/archive.gz'
+
+const targetFilePath = join(currentDir, targetFile)
+const sourceFilePath = join(currentDir, sourceFile)
+
 const decompress = async () => {
-    // Write your code here 
+    const readStream = createReadStream(sourceFilePath)
+    const writeToFileStream = createWriteStream(targetFilePath)
+
+    const decode = createGunzip()
+
+    readStream.pipe(decode).pipe(writeToFileStream)
 };
 
 await decompress();
